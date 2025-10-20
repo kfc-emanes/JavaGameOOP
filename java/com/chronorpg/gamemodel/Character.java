146c3c3674d9c4577a95a659d2f66cdd7a99be94
kfc-emanes <kristcemanes@gmail.com>
2025-10-20 08:59:16 +0800

    Update Character.java

--- java/com/chronorpg/gamemodel/Character.java
+++ java/com/chronorpg/gamemodel/Character.java
@@ -8,9 +8,29 @@ public class Character {
     private int currentHealth;
     private int atk;
     private int def;
-    private int spd; //spd -> used for character attacking order since this is turn-based
+    private int spd; //spd -> used for character attacking order since this is turn-based. many turn-based games use this
 }
 
 public String getName() {
     return name;
+}
+
+public String getMaxHealth() {
+    return maxHealth;
+}
+
+public String getCurrentHealth() {
+    return currentHealth;
+}
+
+public String getAtk() {
+    return atk;
+}
+
+public String getDef() {
+    return def;
+}
+
+public String getSpd() {
+    return spd;
 }
